2024181fd45dd6574376a70b40548c41b28dab4b
iperevozchikov <iperevozchikov@gmail.com>
2018-10-31 00:21:06 +0700

    Generate angular app from cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'td-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'todo-app';
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false
+};
+
+
+import 'zone.js/dist/zone-error';
