75ea0afc1403085aee71642d610f6d7e211f74a0
iperevozchikov <iperevozchikov@gmail.com>
2018-10-31 09:27:30 +0700

    Add firebase configuration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,13 @@
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAuhOscBZW2VhEZn3p_DL0SZyEZxNc1rxg",
+        authDomain: "todoapp-bw.firebaseapp.com",
+        databaseURL: "https://todoapp-bw.firebaseio.com",
+        projectId: "todoapp-bw",
+        storageBucket: "todoapp-bw.appspot.com",
+        messagingSenderId: "983872583803"
+    }
 };
 
 /*
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAuhOscBZW2VhEZn3p_DL0SZyEZxNc1rxg",
+        authDomain: "todoapp-bw.firebaseapp.com",
+        databaseURL: "https://todoapp-bw.firebaseio.com",
+        projectId: "todoapp-bw",
+        storageBucket: "todoapp-bw.appspot.com",
+        messagingSenderId: "983872583803"
+    }
 };
