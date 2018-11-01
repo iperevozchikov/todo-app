bdcf2024f8b53513b7d9969a32887b4655a491e3
iperevozchikov <iperevozchikov@gmail.com>
2018-11-02 02:24:31 +0700

    Update with minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'td-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-    title = 'todo-app';
 }
