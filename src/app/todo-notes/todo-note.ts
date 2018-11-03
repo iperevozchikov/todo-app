b51843af25b471539d98a578fcfd4b26d1c4e2fd
iperevozchikov <iperevozchikov@gmail.com>
2018-11-03 20:57:04 +0700

    Add todo note model

---
+++ src/app/todo-notes/todo-note.ts
@@ -0,0 +1,23 @@
+const Timestamp = require('firebase').firestore.Timestamp;
+
+export enum TodoNoteState {
+    active = 'active',
+    deleted = 'deleted',
+    archived = 'archived'
+}
+
+export interface CheckItem {
+    title: string;
+    done: boolean;
+}
+
+export class TodoNote extends Object {
+    id: string | null = null;
+    userId: string | null = null;
+    title: string = '';
+    checkList: Array<CheckItem> = [];
+    state: TodoNoteState = TodoNoteState.active;
+    createdAt: any = Timestamp.fromDate(new Date());
+    updatedAt: any = Timestamp.fromDate(new Date());
+    deletedAt: any | null = null;
+}
